d4dd69e3454eed83de874d6f78c5f4e592dbd9a7
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-27 15:57:53 +0300

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -148,7 +148,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, bulk=False):
                 return None
         except Exception as e:
             print(e)
-            await client.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`')
+            await client.edit_message_text(sender, edit_id, f'Failed to save: `{e}`')
             if os.path.isfile(file) == True:
                 os.remove(file)
             return None
@@ -166,7 +166,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, bulk=False):
                 return None
         except Exception as e:
             print(e)
-            await client.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`')
+            await client.edit_message_text(sender, edit_id, f'Failed to save: `{e}`')
             return None
         await edit.delete()
         return None
